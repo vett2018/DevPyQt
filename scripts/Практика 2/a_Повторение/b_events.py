b01dac8cb03a084ec08c0029c499829edf4f60db
qq <qq>
2023-05-05 20:31:10 +0300

    init

--- scripts/Практика 2/a_Повторение/b_events.py
+++ scripts/Практика 2/a_Повторение/b_events.py
@@ -8,13 +8,29 @@
 При выводе события указывать время, когда произошло событие.
 """
 
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore, QtGui
+import time
 
 
 class Window(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+
+    def closeEvent(self, event: QtGui.QCloseEvent) -> None: #переопределение событий закрытие
+        print("Close WINDOW")
+
+    def keyPressEvent(self, event: QtGui.QKeyEvent) -> None: #переопределений событий с клавиатуры
+        print(time.ctime(), event)  # перхват событий
+        print(event.text())
+        if event.text() == "H":
+            print("HELLO")
+            #self.destroy()
+        return super().keyPressEvent(event) #что за строчка
+
+    # def event(self, event: QtCore.QEvent) -> bool: #переопределить событие
+    #     print(time.ctime(), event) #перхват событий
+    #     #return super().event(event) #что за строчка
 
 
 if __name__ == "__main__":
