82a84ab24d4bf49f926309499362e50aaad378b0
qq <qq>
2023-05-05 20:08:21 +0300

    init

---
+++ pr2.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'pr2.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDial, QLCDNumber, QLabel,
+    QLineEdit, QMainWindow, QMenuBar, QPushButton,
+    QSizePolicy, QSlider, QStatusBar, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(240, 260, 75, 24))
+        self.lineEdit = QLineEdit(self.centralwidget)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(150, 150, 113, 21))
+        self.lineEdit_2 = QLineEdit(self.centralwidget)
+        self.lineEdit_2.setObjectName(u"lineEdit_2")
+        self.lineEdit_2.setGeometry(QRect(310, 150, 113, 22))
+        self.horizontalSlider = QSlider(self.centralwidget)
+        self.horizontalSlider.setObjectName(u"horizontalSlider")
+        self.horizontalSlider.setGeometry(QRect(150, 380, 160, 22))
+        self.horizontalSlider.setOrientation(Qt.Horizontal)
+        self.dial = QDial(self.centralwidget)
+        self.dial.setObjectName(u"dial")
+        self.dial.setGeometry(QRect(150, 430, 50, 64))
+        self.lineEdit_3 = QLineEdit(self.centralwidget)
+        self.lineEdit_3.setObjectName(u"lineEdit_3")
+        self.lineEdit_3.setGeometry(QRect(450, 200, 113, 22))
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(440, 420, 49, 16))
+        self.lcdNumber = QLCDNumber(self.centralwidget)
+        self.lcdNumber.setObjectName(u"lcdNumber")
+        self.lcdNumber.setGeometry(QRect(370, 490, 64, 23))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 22))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        self.lineEdit.textChanged.connect(self.lineEdit_2.setText)
+        self.horizontalSlider.valueChanged.connect(self.dial.setValue)
+        self.lineEdit_2.textChanged.connect(self.lineEdit_3.setText)
+        self.dial.valueChanged.connect(self.horizontalSlider.setValue)
+        self.horizontalSlider.sliderMoved.connect(self.lcdNumber.display)
+        self.dial.valueChanged.connect(self.lcdNumber.display)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+    # retranslateUi
+
--- scripts/Практика 2/a_Повторение/a_signals.py
+++ scripts/Практика 2/a_Повторение/a_signals.py
@@ -15,6 +15,42 @@ class Window(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.initUi()
+        self.initSignals()
+
+    def initUi(self):
+        self.LineEditInput = QtWidgets.QLineEdit()
+        self.LineEditMirror = QtWidgets.QLineEdit()
+        self.pushButtonMirror = QtWidgets.QPushButton("Отобрази")
+        layout_h = QtWidgets.QHBoxLayout()
+        layout_v = QtWidgets.QVBoxLayout()
+        layout_h.addWidget(self.LineEditInput)
+        layout_h.addWidget(self.LineEditMirror)
+
+        layout_v.addLayout(layout_h)
+        layout_v.addWidget(self.pushButtonMirror)
+
+        self.setLayout(layout_v)
+
+    def initSignals(self):
+        # self.pushButtonMirror.connect(self.pushButtonMirror, "clicked", self.reverseData) # уход от кликедов не получилось
+
+        self.pushButtonMirror.clicked.connect(self.reverseData) #сигнал №1
+        #self.pushButtonMirror.clicked.connect(self.reverseData2) #сигнал №2
+        #self.LineEditInput.textChanged.connect(self.reverseData3) #сигнал №3
+        #self.LineEditInput.textChanged.connect(lambda text: self.LineEditMirror.setText(text[::-1]))
+
+    def reverseData(self):
+        print(data)
+        get_data = self.LineEditInput.text()
+        rev = get_data[::-1]
+        self.LineEditMirror.setText(rev)
+
+    def reverseData2(self, data):
+        self.LineEditMirror.setText(self.LineEditInput.text()[::-1]) #элегантное решение в одну строчку
+
+    def reverseData3(self, text):
+        self.LineEditMirror.setText(text[::-1])
 
 
 if __name__ == "__main__":
