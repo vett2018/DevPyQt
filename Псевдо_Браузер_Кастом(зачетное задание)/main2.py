22382658b10bc52d00e689170d07f2c26d6a77cd
yy <yy>
2023-06-07 23:34:39 +0300

    init

---
+++ Псевдо_Браузер_Кастом(зачетное задание)/main2.py
@@ -0,0 +1,79 @@
+from PySide2 import QtWidgets, QtCore, QtGui
+from PySide2 import QtWebEngineWidgets
+
+from widget2 import Ui_MainWindow  # Импортируем класс формы
+
+
+class Window(QtWidgets.QMainWindow):  # наследуемся от того же класса, что и форма в QtDesigner
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.index = 3
+        self.initUi()
+        self.initSignals()
+
+    def initUi(self):
+        #self.button = QtWidgets.QPushButton("Добавить Вкладку")
+        #self.ui.horizontalLayout.addWidget(self.button)
+        print(self.ui.tab.children())
+        self.tabs = {}
+        #self.tabs["tab"] = {"tab": self.ui.tab, "lineEdit": self.ui.lineEdit, "PushButton": self.ui.pushButton, "Web": self.ui.webEngineView}
+        self.tabs["tab"] = {"tab": self.ui.tab, "Web": self.ui.webEngineView}
+
+        self.tabs["tab_2"] = {"tab": self.ui.tab, "other": []}
+
+
+
+    def initSignals(self):
+        self.ui.pushButton.clicked.connect(self.setUrl)
+        #self.button.clicked.connect(self.createTab)
+        self.ui.pushButton_2.clicked.connect(self.createTab)
+
+    def newTab(self):
+        # eval(f"self.tab_{index}=QtWidgets.QWidget()")
+        tab = QtWidgets.QWidget()
+        verticalLayout_2 = QtWidgets.QVBoxLayout(tab)
+        horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        lineEdit = QtWidgets.QLineEdit(tab)
+        horizontalLayout_2.addWidget(lineEdit)
+
+        pushButton = QtWidgets.QPushButton(tab)
+        icon = QtGui.QIcon()
+        icon.addFile(u":/logo/icons8-\u0434\u0436\u0435\u0439\u043a-16.png", QtCore.QSize(), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        pushButton.setIcon(icon)
+
+        horizontalLayout_2.addWidget(pushButton)
+
+        verticalLayout_2.addLayout(horizontalLayout_2)
+
+        webEngineView = QtWebEngineWidgets.QWebEngineView(tab)
+        webEngineView.setUrl(QtCore.QUrl(u"about:blank"))
+
+        verticalLayout_2.addWidget(webEngineView)
+        self.tabs[f"tab_{self.index}"] = {"tab": tab, "lineEdit": lineEdit, "pushButton": pushButton, "Web": webEngineView}
+        self.index += 1
+        return tab
+
+    def createTab(self):
+        #tab = self.newTab()
+        # print(tab.children())
+        self.ui.tabWidget.addTab(self.newTab(), "Новая Вкладка")
+        tab = self.tabs[f"tab_{self.index - 1}"]
+        #tab["pushButton"].clicked.connect(lambda: tab["Web"].setUrl("https://" + tab["lineEdit"].text()))
+        tab["pushButton"].clicked.connect(lambda: tab["Web"].setUrl("https://ya.ru"))
+
+    def setUrl(self):
+        # self.ui.webEngineView.setUrl("https://" + self.ui.lineEdit.text())
+        self.ui.webEngineView.setUrl("https://ya.ru")
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication()
+
+    window = Window()
+    window.show()
+
+    app.exec_()
+
---
+++ Псевдо_Браузер_Кастом(зачетное задание)/widget2.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'widget2.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+from PySide2.QtWebEngineWidgets import QWebEngineView
+
+
+import widget_rc
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(660, 594)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        icon = QIcon()
+        icon.addFile(u":/logo/icons8-\u0434\u0436\u0435\u0439\u043a-16.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton.setIcon(icon)
+
+        self.horizontalLayout.addWidget(self.pushButton)
+
+        self.pushButton_2 = QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.horizontalLayout.addWidget(self.pushButton_2)
+
+        self.pushButton_3 = QPushButton(self.centralwidget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+
+        self.horizontalLayout.addWidget(self.pushButton_3)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout, 1, 0, 1, 1)
+
+        self.tabWidget = QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.gridLayout_2 = QGridLayout(self.tab)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.webEngineView = QWebEngineView(self.tab)
+        self.webEngineView.setObjectName(u"webEngineView")
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.webEngineView.sizePolicy().hasHeightForWidth())
+        self.webEngineView.setSizePolicy(sizePolicy)
+        self.webEngineView.setProperty("url", QUrl(u"about:blank"))
+
+        self.gridLayout_2.addWidget(self.webEngineView, 1, 0, 1, 1)
+
+        self.tabWidget.addTab(self.tab, "")
+
+        self.gridLayout.addWidget(self.tabWidget, 0, 0, 1, 1)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 660, 21))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        self.tabWidget.setCurrentIndex(0)
+
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"\u041f\u0441\u0435\u0432\u0434\u043e\u0411\u0440\u0430\u0443\u0437\u0435\u0440", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u0414\u043e\u043c\u0430\u0448\u043d\u044f\u044f \u0441\u0442\u0440\u0430\u043d\u0438\u0446\u0430 \u042f\u043d\u0434\u0435\u043a\u0441", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c \u0412\u043a\u043b\u0430\u0434\u043a\u0443", None))
+        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"\u0423\u0434\u0430\u043b\u0438\u0442\u044c \u0412\u043a\u043b\u0430\u0434\u043a\u0443", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("MainWindow", u"\u041d\u0430\u0447\u0430\u043b\u044c\u043d\u0430\u044f \u0412\u043a\u043b\u0430\u0434\u043a\u0430", None))
+    # retranslateUi
+
